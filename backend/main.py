3a265c52a8f306efa08aac581e442dc724b80b0c
Jesus Rico <jesusrico@miw.mx>
2026-04-09 12:36:48 -0600

    cambios en cors

--- backend/main.py
+++ backend/main.py
@@ -29,7 +29,7 @@ app = FastAPI(title="Viernes API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://localhost:4173"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://localhost:4173", "https://sodigic.com", "https://www.sodigic.com", "https://viernes.sodigic.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
